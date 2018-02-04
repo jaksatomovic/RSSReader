98b55e626bd72c714bc9f39de63ee68497a76c0e
Jaksa Tomovic <jaksa.tomovic@gmail.com>
2018-02-04 16:22:42 +0100

    some fix

--- RSSReader/AppDelegate.swift
+++ RSSReader/AppDelegate.swift
@@ -41,18 +41,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
         print("now running in background...")
-        
-        _ = Timer.scheduledTimer(withTimeInterval: 300.0, repeats: true) {
-            (_) in
-            print("now in timer")
+
+        DispatchQueue.global(qos: .background).async {
+            print ("inside timer...")
+            sleep(300)
             let vc = HomeController()
             vc.checkForNewStroies()
+            
         }
     }
-    
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
